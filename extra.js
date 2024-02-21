030b1cd828afc5a319d41b6f8e84323ce9d487c1
Abdur Rahaman Rahi <rahiurp20@gmail.com>
2024-02-21 10:59:51 +0600

    responsive show ticket

---
+++ extra.js
@@ -0,0 +1,8 @@
+let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+
+for(let alp of arr ) {
+    console.log(`<div class="">${alp.toUpperCase()}</div>`)
+    // console.log(`<div class="flex items-center justify-center  w-28 h-14 bg-[#0307121A] rounded-xl">${alp.toUpperCase()}3</div>`)
+    // console.log(`<div class="flex items-center justify-center  w-28 h-14 bg-[#0307121A] rounded-xl">${alp.toUpperCase()}4</div>`)
+}
